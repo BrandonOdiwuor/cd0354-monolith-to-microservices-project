f1ca9fbf329f14a64c47d7579c1aa662e38cf7a6
Brandon Odiwuor <brandon.odiwuor@gmail.com>
2022-07-09 13:22:45 +0300

    Update frontend URL

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a1c4771984fce4f1d927a5b37f1ae099-1206578315.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
